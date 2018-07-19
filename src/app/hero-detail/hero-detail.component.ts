c4758a98092f10bb90bc116d24151417a8aaf62d
rgc <rgc@rafael.goldensoft.local>
2018-07-19 15:05:04 +0200

    Detail View !!  😎

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 
+import { HeroService } from "../hero.service";
+import { ActivatedRoute } from '../../../node_modules/@angular/router';
+import { Location } from '@angular/common';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -10,9 +14,24 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero)
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
